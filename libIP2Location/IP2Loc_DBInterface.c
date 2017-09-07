3ee107d1b962f606727e1664cdc09a0985e9de42
Chris Lim <chrislim2888@yahoo.com>
2017-09-07 13:52:57 +0800

    update license

--- libIP2Location/IP2Loc_DBInterface.c
+++ libIP2Location/IP2Loc_DBInterface.c
@@ -3,18 +3,8 @@
  * Copyright (c) 2013 IP2Location.com. support at ip2location dot com
  *
  * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
+ * modify it under the terms of the MIT license
  * License as published by the Free Software Foundation; either
- * version 3 of the License, or any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not see <http://www.gnu.org/licenses/>.
- *
  */
 
 #ifdef WIN32
--- libIP2Location/IP2Location.h
+++ libIP2Location/IP2Location.h
@@ -3,17 +3,7 @@
  * Copyright (c) 2013-2015 IP2Location.com. support at ip2location dot com
  *
  * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 3 of the License, or any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not see <http://www.gnu.org/licenses/>.
+ * modify it under the terms of the MIT license
  */
 #ifndef HAVE_IP2LOCATION_H
 #define HAVE_IP2LOCATION_H
